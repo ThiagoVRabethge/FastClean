a2fbef4406655856d40567af9557ab5353a4d137
Thiago Volpi Rabethge <89933443+ThiagoVRabethge@users.noreply.github.com>
2023-04-13 01:41:47 +0000

    test

--- src/pages/stores/washsTypesStore.js
+++ src/pages/stores/washsTypesStore.js
@@ -1,5 +1,8 @@
 import { create } from 'zustand';
 
+const test = () => {};
+export default test;
+
 export const useWashsTypesStore = create((set) => ({
   washsTypesList: [],
   setWashsTypesList: (newWashsTypesList) => set((state) => ({ washsTypesList: newWashsTypesList })),
